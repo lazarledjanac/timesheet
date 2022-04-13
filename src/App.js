9b19a23965842c3d251cd5fa8c48fe068e8c31ab
Lazar Ledjanac Praksa <lazar.ledjanac@vega.local>
2022-04-13 09:34:58 +0200

    Create header and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div>
+    <div class="container">
       
+      <Header/>
+    
+
+      <Footer/>
+
     </div>
+  
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer class="footer">
+			<div class="wrapper">
+				<ul>
+					<li>
+						<span>Copyright. VegaITSourcing All rights reserved</span>
+					</li>
+				</ul>
+				<ul class="right">
+					<li>
+						<a href="javascript:;">Terms of service</a>
+					</li>
+					<li>
+						<a href="javascript:;" class="last">Privacy policy</a>
+					</li>
+				</ul>
+			</div>
+		</footer>
+    );
+};
+
+export default Footer;
